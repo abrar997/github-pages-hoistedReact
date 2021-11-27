71dba7d5bd1059fd86b6e82f08942663882805d9
abrar997 <abraralrawi997@gmail.com>
2021-11-27 06:38:26 +0300

    deploy project

--- src/component/Products/ProductItems/ProductItems.js
+++ src/component/Products/ProductItems/ProductItems.js
@@ -14,6 +14,7 @@ import { Link } from "react-router-dom";
 const ProductItems = () => {
   const {addItem}=useCart();
   return (
+    <div className="products-verical">
     <div className="cards">
       {products.data.map((item, ind) => {
         return (
@@ -39,7 +40,7 @@ const ProductItems = () => {
           </div>
         );
       })}
-    </div>
+    </div></div>
   );
 };
 
